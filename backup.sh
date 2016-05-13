3087b7b5462152c9782309c08621893cdbc5ece6
alastweekdahlman <alexdahlman@outlook.com>
2016-05-13 14:53:57 -0600

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Variables
+bucket=s3://staterabackups/Website/Monthly/
+
+# Folder to tar and send
+folder=/opt/bitnami/apps/wordpress/htdocs/
+
+  # Define filenames
+    filename=$(date +'%Y-%m-%d' +)monthlysitebackup.tar.gz
+    tmpfile=/tmp/$filename
+    
+# Dump and zip
+  echo -e "creating \e[0;35m$tmpfile\e[00m"
+
+
+# Script will tar the large htdocs files into a folder call . first. When you're ready to recover,
+# untar the contents of the . folder into the htdocs folder and replace it
+
+tar "$tmpfile" -C "$folder" .
+echo -e "the compression is done.."
+
+# Once archived and moved to /tmp they are moved to the s3 bucket
+
+echo -e "To the cloud Goku!.."
+s3cmd -put "$tmpfile" "$bucket"
+
+# Delete
+rm -f "$tmpfile"
